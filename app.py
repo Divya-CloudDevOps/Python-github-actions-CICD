93850913459fed99c90daed58f2be4df1ba118a1
Divya-CloudDevOps <usercloud003@gmail.com>
2026-01-30 17:32:06 +0530

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-rom flask import Flask
+flask import Flask
 app = Flask(__my-app__)
 
 @app.route("/")
 def home():
     return "Flask app deployed using GitHub Actions!"
 
-if name == "__main__":
-    app.run(host="0.0.0.0", port=5
+__name__ == "__main__":
+    app.run(host="0.0.0.0", port=80
